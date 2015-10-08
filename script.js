02429645e01cad6f70772d3e4f83fed86901b9e8
test <session2@START-Coding-MBP-C02PLK6KG8WN.local>
2015-10-07 17:58:45 -0700

    Added non-working code directly from google charts

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 /**
  * Created by session2 on 10/7/15.
  */
+/*http://factfinder.census.gov/faces/tableservices/jsf/pages/productview.xhtml?src=bkmk*/
+
+google.setOnLoadCallback(drawChart);
+function drawChart() {
+
+    var data;
+    data = google.visualization.arrayToDataTable([
+        ['Function', 'Dollars Spent'],
+        ['Work', 11],
+        ['Eat', 2],
+        ['Commute', 2],
+        ['Watch TV', 2],
+        ['Sleep', 7]
+    ]);
+
+    var options = {
+        title: 'My Daily Activities'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+    chart.draw(data, options);
+}
