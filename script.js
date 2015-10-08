0e88352d158a9ed5c435ff03f9ead32cc8ac2cd7
test <session2@START-Coding-MBP-C02PLK6KG8WN.local>
2015-10-08 16:52:00 -0700

    Finished assignment

--- script.js
+++ script.js
@@ -3,45 +3,32 @@
  */
 /*http://factfinder.census.gov/faces/tableservices/jsf/pages/productview.xhtml?src=bkmk*/
 
-google.load('visualization', '1', {packages: ['corechart', 'bar']});
-google.setOnLoadCallback(drawBasic);
+google.setOnLoadCallback(drawChart);
+function drawChart() {
 
-function drawBasic() {
-
-    var data = new google.visualization.DataTable();
-    data.addColumn('timeofday', 'Time of Day');
-    data.addColumn('number', 'Motivation Level');
-
-    data.addRows([
-        [{v: [8, 0, 0], f: '8 am'}, 1],
-        [{v: [9, 0, 0], f: '9 am'}, 2],
-        [{v: [10, 0, 0], f:'10 am'}, 3],
-        [{v: [11, 0, 0], f: '11 am'}, 4],
-        [{v: [12, 0, 0], f: '12 pm'}, 5],
-        [{v: [13, 0, 0], f: '1 pm'}, 6],
-        [{v: [14, 0, 0], f: '2 pm'}, 7],
-        [{v: [15, 0, 0], f: '3 pm'}, 8],
-        [{v: [16, 0, 0], f: '4 pm'}, 9],
-        [{v: [17, 0, 0], f: '5 pm'}, 10],
+    var data = google.visualization.arrayToDataTable([
+        ['Function', 'US General Expenditure(in millions)'],
+        ['Education', 600],
+        ['Public Welfare', 520],
+        ['Hospitals', 67],
+        ['Health', 63],
+        ['Highways', 112],
+        ['Police Protection', 15],
+        ['Correction', 48],
+        ['Natural Resources', 21],
+        ['Parks and Recreation', 6],
+        ['Governmental Administration', 53],
+        ['Interest on General Debt', 46],
+        ['Other and Unallocable', 133]
     ]);
 
     var options = {
-        title: 'Motivation Level Throughout the Day',
-        hAxis: {
-            title: 'Time of Day',
-            format: 'h:mm a',
-            viewWindow: {
-                min: [7, 30, 0],
-                max: [17, 30, 0]
-            }
-        },
-        vAxis: {
-            title: 'Rating (scale of 1-10)'
-        }
+        title: 'General US Expenditure by Function(in millions)',
+        pieHole: 0.5,
+        backgroundColor: "gray"
     };
 
-    var chart = new google.visualization.ColumnChart(
-        document.getElementById('chart_div'));
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
     chart.draw(data, options);
 }
