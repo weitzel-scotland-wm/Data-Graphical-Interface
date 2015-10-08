0b37c667fb003aa76bb0a5358fc3c782fc311975
test <session2@START-Coding-MBP-C02PLK6KG8WN.local>
2015-10-07 18:05:15 -0700

    Second graph code DID NOT WORK. WHY!?

--- script.js
+++ script.js
@@ -3,24 +3,45 @@
  */
 /*http://factfinder.census.gov/faces/tableservices/jsf/pages/productview.xhtml?src=bkmk*/
 
-google.setOnLoadCallback(drawChart);
-function drawChart() {
+google.load('visualization', '1', {packages: ['corechart', 'bar']});
+google.setOnLoadCallback(drawBasic);
 
-    var data;
-    data = google.visualization.arrayToDataTable([
-        ['Function', 'Dollars Spent'],
-        ['Work', 11],
-        ['Eat', 2],
-        ['Commute', 2],
-        ['Watch TV', 2],
-        ['Sleep', 7]
+function drawBasic() {
+
+    var data = new google.visualization.DataTable();
+    data.addColumn('timeofday', 'Time of Day');
+    data.addColumn('number', 'Motivation Level');
+
+    data.addRows([
+        [{v: [8, 0, 0], f: '8 am'}, 1],
+        [{v: [9, 0, 0], f: '9 am'}, 2],
+        [{v: [10, 0, 0], f:'10 am'}, 3],
+        [{v: [11, 0, 0], f: '11 am'}, 4],
+        [{v: [12, 0, 0], f: '12 pm'}, 5],
+        [{v: [13, 0, 0], f: '1 pm'}, 6],
+        [{v: [14, 0, 0], f: '2 pm'}, 7],
+        [{v: [15, 0, 0], f: '3 pm'}, 8],
+        [{v: [16, 0, 0], f: '4 pm'}, 9],
+        [{v: [17, 0, 0], f: '5 pm'}, 10],
     ]);
 
     var options = {
-        title: 'My Daily Activities'
+        title: 'Motivation Level Throughout the Day',
+        hAxis: {
+            title: 'Time of Day',
+            format: 'h:mm a',
+            viewWindow: {
+                min: [7, 30, 0],
+                max: [17, 30, 0]
+            }
+        },
+        vAxis: {
+            title: 'Rating (scale of 1-10)'
+        }
     };
 
-    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    var chart = new google.visualization.ColumnChart(
+        document.getElementById('chart_div'));
 
     chart.draw(data, options);
 }
